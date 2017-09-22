02e96102591d30c8620e48163616bd7128e0878c
NickvanderSangen <nick.vandersangen@theapsgroup.com>
2017-09-22 13:04:08 +0200

    Angular Controllers & Markup: Handling Events

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -18,22 +18,32 @@ angular.module("eventsApp").controller("EventController", function ($scope){
                 creatorName: 'Bob Smith',
                 duration: '1 hr',
                 level: 'Advanced',
-                abstract: 'In this session you will learn the ins and outs of directives!'
+                abstract: 'In this session you will learn the ins and outs of directives!',
+                upVoteCount: 3
             },
             {
                 name: 'Scopes for fun and profit',                
                 creatorName: 'John Doe',
                 duration: '30 mins',
                 level: 'Introductory',
-                abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.'
+                abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get them to do it for you.',
+                upVoteCount: 2
             },
             {
                 name: 'Well Behaved Controllers',
                 creatorName: 'Jane Doe',
                 duration: '2 hours',
                 level: 'Intermediate',
-                abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.'
+                abstract: 'Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
+                upVoteCount: 35
             }
         ]
+    };
+
+    $scope.upVoteSession = function(session){
+        session.upVoteCount++;
+    };
+    $scope.downVoteSession = function(session){
+        session.upVoteCount--;
     };
 });
